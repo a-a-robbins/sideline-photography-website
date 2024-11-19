a2649730e684af8825ff92db7b90404cabe66492
a-a-robbins <robbinsa1@central.edu>
2024-11-18 19:00:10 -0600

    good spot to check-in, navbar link added

---
+++ sideline-photography-spa/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <navigation-bar :pages="pages" :active-page="activePage"
+    :nav-link-click="(index) => activePage = index"></navigation-bar>
+
+  <page-viewer :page="pages[activePage]"></page-viewer>
+  </template>
+
+<script>
+import PageViewer from './components/PageViewer.vue';
+import NavigationBar from './components/NavigationBar.vue';
+
+export default {
+  components: {
+    NavigationBar, 
+    PageViewer
+  },
+  data() {
+    return {
+      activePage: 0,
+      pages: [
+        { link: { text: 'Home', url: '#' }, pageTitle: 'Home Page', content: 'This is the home page' },
+        { link: { text: 'About', url: '#' }, pageTitle: 'About Page', content: 'A little about Sideline Photography' },
+        { link: { text: 'Contact', url: '#' }, pageTitle: 'Contact Page', content: 'How to get ahold of us' },
+        { link: { text: 'Gallery', url: '#' }, pageTitle: 'Gallery Page', content: 'A beautiful gallery' }
+      ]
+    };
+  }
+}
+</script>
---
+++ sideline-photography-spa/src/components/NavigationBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav :class="navbarClasses">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/sp-logo.png" alt="sideline photo logo" width="30" height="24">
+            </a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                    <navbar-link
+                        :page="page"
+                        :isActive="activePage === index"
+                        @click.prevent="navLinkClick(index)"
+                    ></navbar-link>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <button class="btn btn-danger" @click.prevent="changeTheme">Toggle Navbar</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+import NavbarLink from './NavbarLink.vue';
+
+export default {
+    components: {NavbarLink},
+    props: ['pages', 'activePage', 'navLinkClick'],
+    data() {
+        return {
+            theme: 'dark'
+        };
+    }
+    ,
+    computed: {
+        navbarClasses() {
+            return [`navbar-${this.theme}`, `bg-${this.theme}`, 'navbar', 'navbar-expand-lg'];
+        }
+    },
+    methods: {
+        changeTheme() {
+            this.theme = this.theme === 'light' ? 'dark' : 'light';
+        }
+    }
+}
+</script>
